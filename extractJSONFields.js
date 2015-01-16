edc06f248ed9cca9a50cad93da2a8ce13da6d5fb
ridesoft <maurizio.brioschi@ridesoft.org>
2015-01-16 17:45:29 +0100

    fix

--- extractJSONFields.js
+++ extractJSONFields.js
@@ -2,3 +2,5 @@
 
 var fieldsExpo = require('./lib/extractJSONFields.js');
 fieldsExpo.select('test/data/my/page1.json','.Texts','test.json');
+fieldsExpo.select('test/data/my/page1.json','.Height','height.json');
+fieldsExpo.select('test/data/my/page1.json','.Width','width.json');
