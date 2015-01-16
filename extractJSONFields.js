5960b73d10fafb3b120f6035395b32e8a13f1ac2
= <=>
2015-01-16 17:16:41 +0100

    first extract json field in outputfile

--- extractJSONFields.js
+++ extractJSONFields.js
@@ -1,4 +1,4 @@
 'use strict';
 
 var fieldsExpo = require('./lib/extractJSONFields.js');
-fieldsExpo.find('test/data/my/page1.json','Texts');
+fieldsExpo.select('test/data/my/page1.json','.Texts','test.json');
