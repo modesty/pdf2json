208ddeec7107a3f8cdeed26c9fa63d381d76d63b
ridesoft <maurizio.brioschi@ridesoft.org>
2015-01-16 18:07:01 +0100

    insert optimist

--- extractJSONFields.js
+++ extractJSONFields.js
@@ -2,5 +2,3 @@
 
 var fieldsExpo = require('./lib/extractJSONFields.js');
 fieldsExpo.select('test/data/my/page1.json','.Texts','test.json');
-fieldsExpo.select('test/data/my/page1.json','.Height','height.json');
-fieldsExpo.select('test/data/my/page1.json','.Width','width.json');
