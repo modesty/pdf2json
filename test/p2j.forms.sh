e90431cc2b177aee3d86752cef1f0c0a3d654e1e
modesty.zhang <modesty.zhang@servicenow.com>
2016-02-28 13:45:40 -0800

    start to use es6 with node 4.3

---
+++ test/p2j.forms.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+STARTTIME=$(date +%s)
+AGENCIES=("dc" "de" "ef" "fd" "nd" "or" "pa" "sc" "va")
+for i in "${AGENCIES[@]}"
+do
+	sh ./p2j.one.sh $i
+done
+ENDTIME=$(date +%s)
+echo "It takes $(($ENDTIME - $STARTTIME)) seconds to process all PDFs ..."
