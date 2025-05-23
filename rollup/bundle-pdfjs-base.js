18c7d5b9a203fc909109310d053d6f742bc9d69a
modesty <modestyz@hotmail.com>
2025-05-23 16:32:46 -0700

    maint: remove dependency on @xmldom/xmldom

--- rollup/bundle-pdfjs-base.js
+++ rollup/bundle-pdfjs-base.js
@@ -64,7 +64,7 @@ const _baseCode = _pdfjsFiles.reduce(
 
 fs.writeFileSync(path.join(__dirname, "../lib/pdfjs-code.js"),
 	`
-  ${"import nodeUtil from 'util';import { Blob } from 'buffer';import { DOMParser } from '@xmldom/xmldom';import PDFAnno from './pdfanno.js';import Image from './pdfimage.js';import { createScratchCanvas } from './pdfcanvas.js';"}
+  ${"import nodeUtil from 'util';import { Blob } from 'buffer';import { DOMParser } from './simpleXmlParser.js';import PDFAnno from './pdfanno.js';import Image from './pdfimage.js';import { createScratchCanvas } from './pdfcanvas.js';"}
   ${"export const PDFJS = {};"}
   ${"const globalScope = { console };"}
   ${_baseCode}
