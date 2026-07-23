548d6c32b7f2cb98241cdd9db0ed18b47ce28a6f
petero-dk <2478689+petero-dk@users.noreply.github.com>
2026-07-22 18:06:28 -0700

    fix(types): add rw and rh to Text interface

--- src/types/pdfparser.d.ts
+++ src/types/pdfparser.d.ts
@@ -100,7 +100,8 @@ export declare interface Text {
     x: number;
     y: number;
     w: number;
-    h: number;
+    rw: number;
+    rh: number;
     sw: number;
     A: 'left' | 'center' | 'right';
     R: TextRun[];
