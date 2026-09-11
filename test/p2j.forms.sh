221a5c1006d4d389d1e4e54a20cc4c0b68256b9b
Modesty Zhang <modestyz@hotmail.com>
2026-09-11 11:26:58 -0700

    chore: clean up package.json with test scripts (#435)
    * chore: package.json clean up, deprecated/unnecessary fields are removed and npm scripts simplified
    
    * chore: clean up the testing shell scripts
    
    * chore: clean up test scripts with npm

--- test/p2j.forms.sh
+++ test/p2j.forms.sh
@@ -4,24 +4,12 @@ AGENCIES=("dc" "de" "ef" "fd" "nd" "or" "pa" "sc" "va")
 FAILED_AGENCIES=()
 for i in "${AGENCIES[@]}"
 do
-	sh ./p2j.one.sh $i form "Expected: NO Exception, All Parsed OK"
+	bash ./p2j.one.sh "$i" form
 	STATUS=$?
 	if [ $STATUS -ne 0 ]; then
 		FAILED_AGENCIES+=("$i")
 	fi
 done
-
-# Travis CI doesn't seem to support arrays in bash for testing. 
-# Reverting to a bunch of commands so that build button can be shown.
-# sh ./p2j.one.sh dc
-# sh ./p2j.one.sh de
-# sh ./p2j.one.sh ef
-# sh ./p2j.one.sh fd
-# sh ./p2j.one.sh nd
-# sh ./p2j.one.sh or
-# sh ./p2j.one.sh pa
-# sh ./p2j.one.sh sc
-# sh ./p2j.one.sh va
 
 ENDTIME=$(date +%s)
 echo "It takes $(($ENDTIME - $STARTTIME)) seconds to process all PDFs ..."
